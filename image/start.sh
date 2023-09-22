5a908c96f3466a848ea0cabb2220060edeec354d
Sébastien Deleuze <sdeleuze@vmware.com>
2023-09-22 19:56:39 +0200

    Refinements

--- image/start.sh
+++ image/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -ex
 
+java -XX:SharedArchiveFile=spring-petclinic.dynamic.jsa -XX:+ReplayTraining -XX:+StoreCachedCode -XX:CachedCodeFile=spring-petclinic.code.jsa -XX:CachedCodeMaxSize=512M -DautoQuit=true -cp @petclinic-snapshot/build/unpacked/classpath -Dspring.aot.enabled=true org.springframework.samples.petclinic.PetClinicApplication
 java -XX:SharedArchiveFile=spring-petclinic.dynamic.jsa -XX:+ReplayTraining -XX:+LoadCachedCode -XX:CachedCodeFile=spring-petclinic.code.jsa -Xlog:init -Xlog:scc=error -cp @petclinic-snapshot/build/unpacked/classpath -Dspring.aot.enabled=true org.springframework.samples.petclinic.PetClinicApplication 
