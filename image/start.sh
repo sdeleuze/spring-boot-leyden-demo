ae1d3b8bfda7b4ad827ddac7c76efe0ba7aab116
Sébastien Deleuze <sdeleuze@vmware.com>
2023-09-22 13:42:44 +0200

    Refinements

--- image/start.sh
+++ image/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 
-make run
+java -XX:SharedArchiveFile=spring-petclinic.dynamic.jsa -XX:+ReplayTraining -XX:+LoadCachedCode -XX:CachedCodeFile=spring-petclinic.code.jsa -Xlog:init -Xlog:scc=error -cp @petclinic-snapshot/build/unpacked/classpath -Dspring.aot.enabled=true org.springframework.samples.petclinic.PetClinicApplication 
