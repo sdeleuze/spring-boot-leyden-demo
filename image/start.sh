3bd8c1a249c52fbcb092a129887cc80042ccd4c4
Sébastien Deleuze <sdeleuze@vmware.com>
2023-09-29 17:29:08 +0200

    Polishing

--- image/start.sh
+++ image/start.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 set -ex
 
-java -XX:SharedArchiveFile=spring-petclinic.dynamic.jsa -XX:+ReplayTraining -XX:+StoreCachedCode -XX:CachedCodeFile=spring-petclinic.code.jsa -XX:CachedCodeMaxSize=512M -DautoQuit=true -cp @petclinic-snapshot/build/unpacked/classpath -Dspring.aot.enabled=true org.springframework.samples.petclinic.PetClinicApplication
-java -XX:SharedArchiveFile=spring-petclinic.dynamic.jsa -XX:+ReplayTraining -XX:+LoadCachedCode -XX:CachedCodeFile=spring-petclinic.code.jsa -Xlog:init -Xlog:scc=error -cp @petclinic-snapshot/build/unpacked/classpath -Dspring.aot.enabled=true org.springframework.samples.petclinic.PetClinicApplication 
+# Training run
+java -XX:SharedArchiveFile=spring-petclinic.dynamic.jsa \
+     -XX:+ReplayTraining -XX:+StoreCachedCode \
+     -XX:CachedCodeFile=spring-petclinic.code.jsa \
+     -XX:CachedCodeMaxSize=512M -DautoQuit=true \
+     -cp @petclinic-snapshot/build/unpacked/classpath \
+     -Dspring.aot.enabled=true \
+     org.springframework.samples.petclinic.PetClinicApplication
+
+# Optimized run
+java -XX:SharedArchiveFile=spring-petclinic.dynamic.jsa  \
+     -XX:+ReplayTraining -XX:+LoadCachedCode  \
+     -XX:CachedCodeFile=spring-petclinic.code.jsa \
+     -Xlog:init -Xlog:scc=error  \
+     -cp @petclinic-snapshot/build/unpacked/classpath \
+     -Dspring.aot.enabled=true \
+     org.springframework.samples.petclinic.PetClinicApplication
